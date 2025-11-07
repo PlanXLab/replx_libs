3ad043c513334187fae3bf1c6a2de03fa92e3902
chamin.park <devcamp@gmail.com>
2025-11-07 11:38:49 +0900

    update

--- device/ticle/src/ext/__init__.py
+++ device/ticle/src/ext/__init__.py
@@ -9,6 +9,7 @@ __all__ = (
     "KY022",  "KY022NB", "MPU6050", "MPU6050NB", "Relays", "Servos",  "SR04", "SR04NB", "SR04s", "VL53L0X", "VL53L0XNB", "WS2812Matrix",
 )
 
+# class path for ext package - imports from flattened deployment structure
 _lazy_map = {
     # DOM_MOTION: IMU, accelerometer, gyro
     "MPU6050": (".mpu6050", "MPU6050"),
--- device/ticle/typehints/ext/__init__.pyi
+++ device/ticle/typehints/ext/__init__.pyi
@@ -1,60 +1,31 @@
 __version__ = "1.0.0"
 __author__ = "PlanX Lab Development Team"
 
-import sys
-
+# Type hints for ext package - imports from flattened deployment structure
+from .as5600 import AS5600 as AS5600
+from .as5600_nb import AS5600NB as AS5600NB
+from .bme68x import BME68x as BME68x
+from .bme68x_nb import BME68xNB as BME68xNB
+from .bno055 import BNO055 as BNO055
+from .bno055_nb import BNO055NB as BNO055NB
+from .button import Button as Button
+from .button_nb import ButtonNB as ButtonNB
+from .buttons import Buttons as Buttons
+from .hd44780i2c import HD44780I2c as HD44780I2c
+from .ky022 import KY022 as KY022
+from .ky022_nb import KY022NB as KY022NB
+from .mpu6050 import MPU6050 as MPU6050
+from .mpu6050_nb import MPU6050NB as MPU6050NB
+from .relays import Relays as Relays
+from .servos import Servos as Servos
+from .sr04 import SR04 as SR04
+from .sr04_nb import SR04NB as SR04NB
+from .sr04s import SR04s as SR04s
+from .vl53l0x import VL53L0X as VL53L0X
+from .vl53l0x_nb import VL53L0XNB as VL53L0XNB
+from .ws2812 import WS2812Matrix as WS2812Matrix
 
 __all__ = (
-    "AS5600", "AS5600NB", "BME68x", "BME68xNB", "BNO055", "BNO055NB", "Button",  "ButtonNB", "Buttons", "HD44780I2c", 
-    "KY022",  "KY022NB", "MPU6050", "MPU6050NB", "Relays", "Servos",  "SR04", "SR04NB", "SR04s", "VL53L0X", "VL53L0XNB", "WS2812Matrix",
+    "AS5600", "AS5600NB", "BME68x", "BME68xNB", "BNO055", "BNO055NB", "Button", "ButtonNB", "Buttons", "HD44780I2c",
+    "KY022", "KY022NB", "MPU6050", "MPU6050NB", "Relays", "Servos", "SR04", "SR04NB", "SR04s", "VL53L0X", "VL53L0XNB", "WS2812Matrix",
 )
-
-_lazy_map = {
-    # DOM_MOTION: IMU, accelerometer, gyro
-    "MPU6050": (".mpu6050", "MPU6050"),
-    "MPU6050NB": (".mpu6050_nb", "MPU6050NB"),
-    "BNO055": (".bno055", "BNO055"),
-    "BNO055NB": (".bno055_nb", "BNO055NB"),
-    # DOM_ORIENT: Encoders, magnetometer
-    "AS5600": (".as5600", "AS5600"),
-    "AS5600NB": (".as5600_nb", "AS5600NB"),
-    # DOM_DIST: Ultrasonic, ToF, IR
-    "SR04": (".sr04", "SR04"),
-    "SR04NB": (".sr04_nb", "SR04NB"),
-    "SR04s": (".sr04s", "SR04s"),
-    "VL53L0X": (".vl53l0x", "VL53L0X"),
-    "VL53L0XNB": (".vl53l0x_nb", "VL53L0XNB"),
-    # DOM_ENV: Temp, humidity, pressure, gas
-    "BME68x": (".bme68x", "BME68x"),
-    "BME68xNB": (".bme68x_nb", "BME68xNB"),
-    # DOM_INPUT + DOM_EVENT: Buttons, IR, touch
-    "Button": (".button", "Button"),
-    "ButtonNB": (".button_nb", "ButtonNB"),
-    "Buttons": (".buttons", "Buttons"),
-    "KY022": (".ky022", "KY022"),
-    "KY022NB": (".ky022_nb", "KY022NB"),
-    # Actuators: Motors, servos, relays
-    "Relays": (".relays", "Relays"),
-    "Servos": (".servos", "Servos"),
-    # Display: LCD, OLED, LED Matrix
-    "HD44780I2c": (".hd44780i2c", "HD44780I2c"),
-    "WS2812Matrix": (".ws2812", "WS2812Matrix"),
-}
-
-
-def __getattr__(name):
-    try:
-        rel_mod, attr = _lazy_map[name]
-    except KeyError:
-        raise AttributeError(f"module {__name__!r} has no attribute {name!r}") from None
-    
-    fullname = __name__ + rel_mod
-    __import__(fullname)
-    mod = sys.modules[fullname]
-        
-    obj = getattr(mod, attr)
-    globals()[name] = obj
-    return obj
-
-def __dir__():
-    return sorted(list(globals().keys()) + list(_lazy_map.keys()))
